fab48f7c40852a287e5cd265b9d7b1a277959e41
LuHao <lhzbxx@sjtu.edu.cn>
2015-10-31 16:32:08 +0800

    Init.
    Signed-off-by: LuHao <lhzbxx@sjtu.edu.cn>

---
+++ application.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+# 
+# RESTful-API
+# 
+# 
+
+#########
+#
+# 模块导入&初始化
+# 
+#########
+from flask import *
+import sqlite3
+import os
+import time
+import uuid
+
+app = Flask(__name__)
+#
+#
+#
+#########
+
+#########
+#
+# 0. 测试
+# 
+#########
+@app.route('/test/api/hello')
+def test_hello():
+	r = {'status': 100, "msg": "Hello!"}
+	return jsonify(r)
+@app.route('/test/api/post', methods=['POST'])
+def test_post():
+	if not request.args:
+		r = {'status': 200, "msg": "缺少参数"}
+	else:
+		r = {'status': 100, "msg": request.args.get('param', '')}
+	return jsonify(r)
+@app.route('/test/WorkWatcher/listen_work', methods=['POST'])
+def WW_listen():
+	return "success"
+@app.route('/test/WorkWatcher/upload_image', methods=['GET', 'POST'])
+def WW_upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        file.save(os.path.join("img", file.filename))
+        return "success"
+#
+#
+#
+#########
+
+#########
+#
+# 前端页面
+# 
+#########
+@app.route('/')
+def index_page():
+	return render_template("index.html")
+@app.route('/search')
+def search_page():
+	return render_template("list.html")
+@app.route('/detail')
+def detail_page():
+	return render_template("detail.html")
+@app.route('/success')
+def success_page():
+	return render_template("success.html")
+@app.route('/order')
+def order_page():
+	if session.get('user'):
+		return render_template("order.html")
+	else:
+		return redirect(url_for('login_page'))
+@app.route('/pay')
+def pay_page():
+	if session.get('user'):
+		return render_template("pay.html")
+	else:
+		return redirect(url_for('login_page'))
+@app.route('/login')
+def login_page():
+	session['user'] = "login"
+	return render_template("login.html")
+#
+#
+#
+#########
+
+#########
+#
+# 后台函数
+# 
+#########
+@app.route('/login-back')
+def login():
+	# 登录成功。
+	return jsonify({"data": 100})
+	# 用户名不存在。
+	return jsonify({"data": 101})
+	# 密码错误。
+	return jsonify({"data": 102})
+@app.route('/register-back')
+def register():
+	# 注册成功。
+	return jsonify({"data": 100})
+	# 用户名已存在。
+	return jsonify({"data": 101})
+	# 验证码错误。
+	return jsonify({"data": 102})
+#
+#
+#
+#########
+
+#########
+#
+# 基础组件&函数
+# 
+#########
+app.config.update(dict(
+	DATABASE=os.path.join(app.root_path, 'db/main.db'),
+	DEBUG=True,
+	# 生成秘钥
+	SECRET_KEY=os.urandom(24),
+	USERNAME='admin',
+	PASSWORD='default'
+))
+def connect_db():
+	return sqlite3.connect(app.config['DATABASE'])
+@app.before_request
+def before_request():
+	g.db = connect_db()
+@app.teardown_request
+def teardown_request(exception):
+	g.db.close()
+# 简化sqlite3的查询方式。
+def query_db(query, args=(), one=False):
+	cur = g.db.execute(query, args)
+	rv = [dict((cur.description[idx][0], value) for idx, value in enumerate(row)) for row in cur.fetchall()]
+	return (rv[0] if rv else None) if one else rv
+
+
+if __name__ == '__main__':
+	app.run(debug = True)
+#
+#
+#
+#########
---
+++ schema.sql
@@ -0,0 +1,143 @@
+drop table if exists users;
+create table users (
+    id integer primary key autoincrement,
+    uuid integer unique,
+    user_name text not null,
+    user_passwd text not null,
+    phone_number integer not null,
+    register_time integer not null,
+    history_order_num integer default 0
+);
+
+drop table if exists merchants;
+create table merchants (
+    id integer primary key autoincrement,
+    uuid integer unique,
+    merchant_name text not null,
+    merchant_passwd text not null,
+    merchant_description text,
+    merchant_remark text,
+    phone_number integer not null,
+    register_time integer not null,
+    money_number integer default 0,
+    check_time integer default 0,
+    merchant_number integer default 0
+);
+
+drop table if exists photoes;
+create table photoes (
+    id integer primary key autoincrement,
+    uuid integer unique,
+    url text,
+    user_uuid integer,
+    room_uuid integer,
+    foreign key(user_uuid) references users(uuid),
+    foreign key(room_uuid) references rooms(uuid)
+);
+
+drop table if exists orders;
+create table orders (
+    id integer primary key autoincrement,
+    uuid integer unique,
+    user_uuid integer not null,
+    room_uuid integer not null,
+    deal_time integer not null,
+    deal_state integer default 0,
+    foreign key(user_uuid) references users(uuid),
+    foreign key(room_uuid) references rooms(uuid)
+);
+
+drop table if exists coupons;
+create table coupons (
+    id integer primary key autoincrement,
+    uuid integer unique,
+    user_uuid integer not null,
+    coupon_type integer not null,
+    coupon_state integer default 0,
+    foreign key(user_uuid) references users(uuid)
+);
+
+drop table if exists places;
+create table places (
+    id integer primary key autoincrement,
+    uuid integer unique,
+    place_name text not null
+);
+
+drop table if exists rooms;
+create table rooms (
+    id integer primary key autoincrement,
+    uuid integer unique,
+    merchant_uuid integer not null,
+    room_name text not null,
+    room_price text not null,
+    room_remark1 text,
+    room_remark2 text,
+    room_type integer default 0,
+    place_uuid integer not null,
+    room_description text not null,
+    room_coordinate integer,
+    device_network integer default 0,
+    device_shower integer default 0,
+    device_ct integer default 0,
+    device_ac integer default 0,
+    device_sheet integer default 0,
+    device_tv integer default 0,
+    device_drink integer default 0,
+    device_brush integer default 0,
+    device_towel integer default 0,
+    device_slipper integer default 0,
+    device_toilet integer default 0,
+    device_shampoo integer default 0,
+    device_pc integer default 0,
+    device_smoke integer default 0,
+    star_average integer default 0,
+    stock integer default 0,
+    foreign key(merchant_uuid) references merchants(uuid),
+    foreign key(place_uuid) references places(uuid)
+);
+
+drop table if exists comments;
+create table comments (
+    id integer primary key autoincrement,
+    uuid integer unique,
+    star integer not null,
+    user_uuid integer not null,
+    room_uuid integer not null,
+    content text,
+    foreign key(user_uuid) references users(uuid),
+    foreign key(room_uuid) references rooms(uuid)
+);
+
+drop table if exists advice;
+create table advice (
+    id integer primary key autoincrement,
+    uuid integer unique,
+    user_uuid integer not null,
+    content text not null,
+    advice_time integer not null,
+    foreign key(user_uuid) references users(uuid)
+);
+
+drop table if exists total;
+create table total (
+    total_order integer default 0,
+    total_money integer default 0,
+    total_user integer default 0,
+    total_merchant integer default 0,
+    total_room integer default 0,
+    total_advice integer default 0,
+    total_coupon integer default 0
+);
+
+drop table if exists log;
+create table log (
+    time integer primary key,
+    log_order integer default 0,
+    log_money integer default 0,
+    log_user integer default 0,
+    log_merchant integer default 0,
+    log_room integer default 0,
+    log_advice integer default 0,
+    log_coupon integer default 0
+);
