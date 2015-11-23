791f2377946a06bc89471b9bd084d4e07cbd1293
LuHao <lhzbxx@sjtu.edu.cn>
2015-11-23 18:22:31 +0800

    Signed-off-by: LuHao <lhzbxx@sjtu.edu.cn>

--- application.py
+++ application.py
@@ -884,7 +884,7 @@ def get_weixin_user_code(id):
 	# debug = '127.0.0.1'
 	param = urllib.urlencode({'id': id})
 	debug = 'www.qilefun.com%2Fpay%3F' + param
-	url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxfee84b23a06c2b97&redirect_uri=https%3A%2F%2Fwww.qilefun.com%2Fpay%2F&response_type=code&scope=snsapi_base&state=' + id  + '#wechat_redirect'
+	url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxfee84b23a06c2b97&redirect_uri=https%3A%2F%2Fwww.qilefun.com%2Fpay%3F' + id + '&response_type=code&scope=snsapi_base&state=' + id  + '#wechat_redirect'
 	# url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx520c15f417810387&redirect_uri=https%3A%2F%2Fchong.qq.com%2Fphp%2Findex.php%3Fd%3D%26c%3DwxAdapter%26m%3DmobileDeal%26showwxpaytitle%3D1%26vb2ctag%3D4_2030_5_1194_60&response_type=code&scope=snsapi_base&state=123#wechat_redirect'
 	print '>>>get first step code: ' + url
 	return url
