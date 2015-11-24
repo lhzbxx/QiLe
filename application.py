f24c03aa03bdf8ffd7bbcf0aa748d2a2f1785b64
LuHao <lhzbxx@sjtu.edu.cn>
2015-11-25 06:55:26 +0800

    Signed-off-by: LuHao <lhzbxx@sjtu.edu.cn>

--- application.py
+++ application.py
@@ -874,7 +874,7 @@ def pay_success():
 	user = query_db('select * from users where uuid = ?', [s.login], one=True)
 	t1 = request.form.get("t1")
 	order = query_db('select * from orders where uuid = ?', [t1], one=True)
-	room = query_db('select merchant_uuid, room_name from rooms where uuid = ?', [order['room_uuid']], one=True)
+	room = query_db('select merchant_uuid, room_name, stock from rooms where uuid = ?', [order['room_uuid']], one=True)
 	merchant = query_db('select * from merchants where uuid = ?', [room['merchant_uuid']], one=True)
 	t1 = timedate2int(order['date1'])
 	t2 = timedate2int(order['date2'])
