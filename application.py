9db5f07a13525ea45da00597e1fb93453114b0e9
LuHao <lhzbxx@sjtu.edu.cn>
2015-11-27 19:37:32 +0800

    Signed-off-by: LuHao <lhzbxx@sjtu.edu.cn>

--- application.py
+++ application.py
@@ -1055,7 +1055,7 @@ def pay_success():
 		raise e
 		return jsonify({"data": 103})
 	try:
-		send_sms(user['phone_number'], '订单确认：' + liverstr(order['liver_info']) + '在' + str(order['date1']) + '至' + str(order['date2']) + '入住' + str(merchant['merchant_name']) + str(room['room_name']) + str(timedate2timedelta(order['date2'], order['date1'])) + '晚' + '总价：￥' + str(order['deal_price']) + '地址：' + str(order['order_address']) + '电话：' + str(merchant['merchant_phone_number1']) + '其乐客服：4000125176，关注微信公众号其乐，更多惊喜等待你！')
+		send_sms(user['phone_number'], '订单确认：' + liverstr(order['liver_info']) + '在' + str(order['date1']) + '至' + str(order['date2']) + '入住' + str(merchant['merchant_name']) + str(room['room_name']) + str(timedate2timedelta(order['date2'], order['date1'])) + '晚' + '总价：￥' + str(order['deal_price']) + '地址：' + str(room['room_address']) + '电话：' + str(merchant['merchant_phone_number1']) + '其乐客服：4000125176，关注微信公众号其乐，更多惊喜等待你！')
 	except Exception, e:
 		raise e
 		return jsonify({"data": 104})
