2e67b4081dc6713605c9c670fe673d4ff3c51919
LuHao <lhzbxx@sjtu.edu.cn>
2015-12-16 17:04:54 +0800

    Signed-off-by: LuHao <lhzbxx@sjtu.edu.cn>

--- application.py
+++ application.py
@@ -186,7 +186,7 @@ def order_page(id):
 		return redirect(url_for('index_page'))
 	room = query_db('select * from rooms where uuid = ?', [id], one=True)
 	checkins = query_db('select * from user_checkin where user_uuid = ?', [s.login])
-	if room['room_price'] == 19.9
+	if room['room_price'] == 19.9:
 		coupons = query_db('select * from coupons where phone_number = ? ORDER BY id DESC', [user['phone_number']])
 		for coupon in coupons:
 			coupon['limit_time'] = time.strftime("%Y-%m-%d", time.localtime(coupon['limit_time']))
